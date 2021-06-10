c3d1518db5265e20d4b6dba8c5728e316ea90e49
LuukEbenau <l.ebenau@hotmail.com>
2021-06-11 00:39:31 +0200

    k

--- LetsChess-Functions/Function1.cs
+++ LetsChess-Functions/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace LetsChess.Functions
+namespace LetsChess_Functions
 {
     public static class Function1
     {
